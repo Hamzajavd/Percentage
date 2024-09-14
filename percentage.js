1571119d38ae632257cea99b7a6d39a30ea8cf5b
Hamza Javd <javedhamza895@gmail.com>
2024-09-15 02:49:14 +0500

    Assignment 3

---
+++ percentage.js
@@ -0,0 +1,12 @@
+let num = prompt ("Enter a Percentage") 
+    if (num >= 90) {
+        console.log ("A Grade");
+    } else if (num >= 80) {
+        console.log ("B Grade");
+    } else if (num >= 70) {
+        console.log ("C Grade");
+    } else if (num >= 60) {
+        console.log ("D Grade");
+    } else {
+        console.log ("Fail");
+    }
